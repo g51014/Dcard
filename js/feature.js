3b6500a15d873095ba098b1e281bc1d03a201fbc
g51014 <a912686931@gmail.com>
2017-05-14 23:44:31 +0800

    getweb

---
+++ js/feature.js
@@ -0,0 +1,32 @@
+$(".home").click(function(){
+  $(".home span").toggle(500);
+  // $(".home p").append("<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>");
+  $(".home_text").slideToggle(500);
+});
+// $(".home").click(function(){
+//   $(".home_text").slideUp(500);
+// });
+
+$(".ntut").click(function(){
+  $(".ntut span").toggle(500);
+  // $(".home p").append("<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>");
+  $(".ntut_text").slideToggle(500);
+});
+
+$(".CSIE").click(function(){
+  $(".CSIE span").toggle(500);
+  // $(".home p").append("<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>");
+  $(".CSIE_text").slideToggle(500);
+});
+
+$(".train").click(function(){
+  $(".train span").toggle(500);
+  // $(".home p").append("<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>");
+  $(".train_text").slideToggle(500);
+});
+
+$(".web").click(function(){
+  $(".web span").toggle(500);
+  // $(".home p").append("<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>");
+  $(".web_text").slideToggle(500);
+});
